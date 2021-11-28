ae12b29f06ab777e4ab206878a7cae31b308ade7
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2021-11-27 17:48:56 -0800

    Inital Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+function onRequest(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("You have a website.");
+    res.end();
+}
+
+http.createServer(onRequest).listen(8000);
+console.log("Server available");
+
