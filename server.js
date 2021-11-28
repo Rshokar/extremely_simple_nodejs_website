43866141bb21eea2158163ce0275829e8bda42de
Rshokar <60485121+Rshokar@users.noreply.github.com>
2021-11-27 18:57:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ function onRequest(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write("You have a website.");
     res.end();
+    console.log("Succesfull request.");
 }
 
 http.createServer(onRequest).listen(8000);
